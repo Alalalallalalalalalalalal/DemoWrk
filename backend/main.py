e047717d4a8115539973834a6e237a8135d14f07
elkaym23 <elkaymiller@gmail.com>
2026-06-03 23:02:27 -0500

    fix import path

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.postgres.analytics import router as analytics_router
+from postgres.analytics import router as analytics_router
 
 app = FastAPI()
 
