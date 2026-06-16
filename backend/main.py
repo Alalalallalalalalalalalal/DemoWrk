3e0aa9dc7c14a01e7dccbfd97b84f7cb29bcc4c9
lanna_lalala;- <147015359+Alalalallalalalalalalalal@users.noreply.github.com>
2026-06-16 13:25:10 -0500

    update link

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://intern-test-site.netlify.app"],
+    allow_origins=["http://localhost:5173", "https://interntestsite.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
